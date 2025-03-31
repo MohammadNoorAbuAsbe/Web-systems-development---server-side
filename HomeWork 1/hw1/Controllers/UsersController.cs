02ef5a3cbfce61d60b148df6951e468cac2abbea
MohammadNoorAbuAsbe <93922145+MohammadNoorAbuAsbe@users.noreply.github.com>
2025-03-31 14:28:12 +0300

    Fixed UsersController

--- HomeWork 1/hw1/Controllers/UsersController.cs
+++ HomeWork 1/hw1/Controllers/UsersController.cs
@@ -4,7 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace hw1.Controllers
 {
-    public class UsersController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
     {
         [HttpGet]
         public IEnumerable<User> Get()
@@ -16,81 +18,6 @@ namespace hw1.Controllers
         public bool Post([FromBody] User newUser)
         {
             return Models.User.Insert(newUser);
-        }
-
-        // GET: UsersController
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        // GET: UsersController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: UsersController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: UsersController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: UsersController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: UsersController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: UsersController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: UsersController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
