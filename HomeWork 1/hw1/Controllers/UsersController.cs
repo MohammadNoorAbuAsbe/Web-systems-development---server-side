f3e49078049f23dc179fd55ae81c64577e969fd2
Roaa <roaakhashab1@gmail.com>
2025-03-31 13:51:25 +0300

    Roaa's sol

---
+++ HomeWork 1/hw1/Controllers/UsersController.cs
@@ -0,0 +1,96 @@
+﻿using hw1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hw1.Controllers
+{
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return Models.User.Read();
+        }
+
+        [HttpPost]
+        public bool Post([FromBody] User newUser)
+        {
+            return Models.User.Insert(newUser);
+        }
+
+        // GET: UsersController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UsersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ HomeWork 1/hw1/Models/User.cs
@@ -0,0 +1,46 @@
+﻿namespace hw1.Models
+{
+    public class User
+    {
+        int Id;
+        string Name;
+        string Email;
+        string Password;
+        bool Active = true;
+        static List<User> UsersList = new List<User>();
+
+        public User(int id1, string name1, string email1, string password1, bool active1)
+        {
+            Id1 = id1;
+            Name1 = name1;
+            Email1 = email1;
+            Password1 = password1;
+            Active1 = active1;
+        }
+
+        public User() { }
+        public int Id1 { get => Id; set => Id = value; }
+        public string Name1 { get => Name; set => Name = value; }
+        public string Email1 { get => Email; set => Email = value; }
+        public string Password1 { get => Password; set => Password = value; }
+        public bool Active1 { get => Active; set => Active = value; }
+
+        public static List<User> Read()
+        {
+            return UsersList;
+        }
+
+        public static bool Insert(User user)
+        {
+            foreach (var existingUser in UsersList)
+            {
+                if (existingUser.Id1 == user.Id1)
+                {
+                    return false; 
+                }
+            }
+            UsersList.Add(user);
+            return true;
+        }
+    }
+}
