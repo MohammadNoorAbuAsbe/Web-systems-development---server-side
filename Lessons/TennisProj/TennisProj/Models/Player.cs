fc586cf81ccbde3a2688654d72eabb5074fe0a18
MohammadNoorAbuAsbe <93922145+MohammadNoorAbuAsbe@users.noreply.github.com>
2025-04-03 14:54:53 +0300

    jQuery and Tennis Added

---
+++ Lessons/TennisProj/TennisProj/Models/Player.cs
@@ -0,0 +1,41 @@
+﻿namespace TennisProj.Models
+{
+    public class Player
+    {
+        string name;
+        double age;
+        double winRate;
+
+        static List<Player> playerList= new List<Player>();
+
+        public Player(string name, double age, double winRate)
+        {
+            Name = name;
+            Age = age;
+            WinRate = winRate;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public double Age { get => age; set => age = value; }
+        public double WinRate { get => winRate; set => winRate = value; }
+
+
+        Player() { }
+
+        public int Insert() {
+
+            playerList.Add(this);
+            return 1;
+        
+        }
+
+        static public List<Player> Read()
+        {
+            return playerList;
+        }
+
+    }
+
+
+
+}
